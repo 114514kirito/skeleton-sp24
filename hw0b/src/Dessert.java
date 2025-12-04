0807f8251cf6641040ea26dc537bb00f74d191b0
kirito <2033982912@qq.com>
2025-12-04 21:12:18 +0800

    111

---
+++ hw0b/src/Dessert.java
@@ -0,0 +1,24 @@
+import java.text.Format;
+
+public class Dessert {
+    int flavor;
+    int price;
+    public Dessert(int flavor,int price)
+    {
+        this.flavor=flavor;
+        this.price=price;
+    }
+    static int numDesserts=0;
+    public void printDessert()
+    {
+        numDesserts++;
+        String s=String.format("%d %d %d",this.flavor,this.price,numDesserts);
+        System.out.println(s);
+    }
+
+    static void main() {
+        System.out.println("I love dessert !");
+    }
+
+
+}
