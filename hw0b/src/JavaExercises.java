688754a589eb7b75f2123f67aeeb6379dffba00c
kirito <2033982912@qq.com>
2025-12-04 20:21:30 +0800

    111

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -6,7 +6,7 @@ public class JavaExercises {
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
         // TODO: Fill in this function.
-        return null;
+        return new int[]{1,2,3,4,5,6};
     }
 
     /** Returns the order depending on the customer.
@@ -15,14 +15,32 @@ public class JavaExercises {
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
         // TODO: Fill in this function.
-        return null;
+        if(customer.equals("Ergun")){
+            return new String[]{"beyti", "pizza", "hamburger", "tea"};
+        }
+        else if(customer.equals("Erik")){
+            return new String[]{"beyti", "pizza", "hamburger", "tea"};
+        }
+        else {
+            return new String[3];
+        }
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
         // TODO: Fill in this function.
-        return 0;
+        if (array == null || array.length == 0) {
+            throw new IllegalArgumentException("Array must not be empty");
+        }
+       int min=array[0];
+       int max=array[0];
+       for (int j:array)
+       {
+           if(min >j) min=j;
+           if(max <j) max=j;
+       }
+        return max-min;
     }
 
     /**
@@ -39,7 +57,16 @@ public class JavaExercises {
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
         // TODO: Fill in this function.
-        return null;
+        if (x == 1) {
+            list.add(x);
+        } else if (x % 2 == 0) {
+            list.add(x);
+            return hailstone(x / 2);
+        } else if (x % 2 != 0) {
+            list.add(x);
+            return hailstone(x * 3 + 1);
+        }
+        return list;
     }
 
 }
